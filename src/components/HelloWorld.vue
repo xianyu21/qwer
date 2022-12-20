579a7bddf5990f59bbbe868627a0ec5b6275e20b
LeeZi <35288214+xianyu21@users.noreply.github.com>
2022-12-20 12:18:56 +0800

    foo: this will fail

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter11111111111111111111111111</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News按时大大多多多多多多多多多多多多多多</a></li>
     </ul>
     <h3>Ecosystem</h3>
